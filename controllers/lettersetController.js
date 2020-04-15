85f2d868e486159f94c3290d576f22626d6f4aa7
shininglite <vandeusent@gmail.com>
2020-04-15 18:56:18 -0500

    configure config.json with production environment variable JAWSDB_URL

--- controllers/lettersetController.js
+++ controllers/lettersetController.js
@@ -4,8 +4,7 @@ const db = require('../models');
 
 router.get('/api/lettersets', (req, res) => {
   db.Letterset.findAll({
-    // INNER JOIN on scores
-    //include: [db.Score]
+  
   })
   .then(results => res.json(results))
   .catch(error => res.json(error))
@@ -16,20 +15,14 @@ router.get('/api/letterset/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    // INNER JOIN on 
-   // include: [db.Book]
+ 
   })
   .then(results => res.json(results))
   .catch(error => res.json(error))
 });
 
 router.post('/api/letterset', (req, res) => {
-  // I uncommented this with Erik
-    // req.body = {
-    //   email: 'J.K.',
-    //   password: 'Rowling'
-    // }
-   
+    
   db.Letterset.create(req.body)
   .then((response) => res.status(200).json(response))
   .catch(error => res.status(500).json(error))
