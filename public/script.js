5b74d116bf88071fee9b494aefbe7347a40ca116
shininglite <vandeusent@gmail.com>
2020-04-19 16:50:22 -0500

    wordguess function more progress

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-//require("dotenv").config();
-//const apikey = process.env.API_KEY;
-var str = "";
+// require("dotenv").config();
+// const apikey = process.env.API_KEY;
+// var str = "";
 // var arrBlastro = [];
 
 $(document).ready(function () {
@@ -11,30 +11,30 @@ $(document).ready(function () {
   })
 
   $("#blastro").on("click", function () {
-      $("#currentLetterset").empty();
-      var lettersetOne = $("#currentLetterset");
-      var lettersetDiv = $("<span>").text("blastro");
-      // var btnId = "btnId" + 0;
-      $(lettersetOne).append(lettersetDiv);
-      // dynamically create Bootstrap buttons using jQuery
-      // iterate around the letterset
-        var blastro = "blastro";
-      for (var i = 0; i < blastro.length; i++) {
-        var arrBlastro = blastro[i]; 
-        console.log(arrBlastro);
-        var charDiv = $("#charDiv");
-        var charBtn = $("<button>").attr("id", "btnId" + arrBlastro).attr("type", "button").attr("class", "btn btn-secondary").text(arrBlastro);
-        $(charDiv).append(charBtn);
+    $("#currentLetterset").empty();
+    var lettersetOne = $("#currentLetterset");
+    var lettersetDiv = $("<span>").text("blastro");
+    // var btnId = "btnId" + 0;
+    $(lettersetOne).append(lettersetDiv);
+    // dynamically create Bootstrap buttons using jQuery
+    // iterate around the letterset
+    var blastro = "blastro";
+    for (var i = 0; i < blastro.length; i++) {
+      var arrBlastro = blastro[i];
+      console.log(arrBlastro);
+      var charDiv = $("#charDiv");
+      var charBtn = $("<button>").attr("id", "btnId" + arrBlastro).attr("type", "button").attr("class", "btn btn-secondary").text(arrBlastro);
+      $(charDiv).append(charBtn);
 
-        createWordGuess();
-        function createWordGuess(){
-          $("#btnId" + arrBlastro[i]).on("click", function () {
-            console.log("#btnId" + arrBlastro);
-          })
-        }
+      createWordGuess();
+      function createWordGuess() {
+        $("#btnId" + arrBlastro[i]).on("click", function () {
+          console.log("#btnId" + arrBlastro);
+        })
       }
+    }
   });
-//end of document ready function  
+  //end of document ready function  
 });
 
 function wordCheck() {
@@ -58,12 +58,11 @@ function wordCheck() {
   })
 }
 
+/* Function order of execution
 
+function validWord({
+  add currentWord to div
+  display div to wordgame.html using jQuery
+  TODO: dynamically create ul, li list of valid words
 
-
-// function validWord({
-//   // add currentWord to div
-//   // display div to wordgame.html using jQuery
-//   // TODO: dynamically create ul, li list of valid words
-
-// })
+*/
